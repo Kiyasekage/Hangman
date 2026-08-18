5037b6729b475c748151999c9f8144c4beadd311
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-08-19 01:24:28 +0800

    add the foundation for guessing the word

--- file.py
+++ file.py
@@ -2,3 +2,13 @@
 print(f"    +{"-"*3}+")
 print(f"{"    |\n"*3}    |")
 print("="*10)
+#program
+
+import random
+word = ["UDEMY","LOVER"]
+secret = random.choice(word)
+longity = len(secret)
+blank = []
+for space in range(longity):
+    blank.append("_")
+print(blank)
