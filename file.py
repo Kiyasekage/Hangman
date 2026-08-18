cdf1a99b314e4c8812ca9790d43ba4134039c4d7
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-08-19 01:49:46 +0800

    add guess system and revealing letters when guessed

--- file.py
+++ file.py
@@ -3,7 +3,6 @@ print(f"    +{"-"*3}+")
 print(f"{"    |\n"*3}    |")
 print("="*10)
 #program
-
 import random
 word = ["UDEMY","LOVER"]
 secret = random.choice(word)
@@ -11,4 +10,15 @@ longity = len(secret)
 blank = []
 for space in range(longity):
     blank.append("_")
+print(blank)
+guess = input("Guess a letter : ").upper()
+guessed = []
+if guess in guessed:
+    print("You have already guessed this letter!")
+else:
+    guessed.append(guess)
+for position in range(longity):
+    letter = secret[position]
+    if guess==letter:
+        blank[position] = letter
 print(blank)
