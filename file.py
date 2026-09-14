fe044ad97ef03c6d00a5ff1bbf126b5a08e54b9f
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-09-15 05:14:15 +0800

    tidy up the guess input interface,add user's guessed letters, add 9 phase hangman drawing

--- file.py
+++ file.py
@@ -1,8 +1,69 @@
-#first three sequence
-print(f"    +{"-"*3}+")
-print(f"{"    |\n"*3}    |")
-print("="*10)
-#program
+hangman_stages = ['''
+    +---+
+    |   |
+    |   0
+    |  /|\
+    |  / \
+    |
+=============''','''
+    +---+
+    |   |
+    |   0
+    |  /|\
+    |  / 
+    |
+=============''','''
+    +---+
+    |   |
+    |   0
+    |  /|\
+    |  
+    |
+=============''','''
+    +---+
+    |   |
+    |   0
+    |   |
+    |
+    |
+=============''','''
+    +---+
+    |   |
+    |   0
+    |
+    |
+    |
+=============''','''
+    +---+
+    |   |
+    |
+    |
+    |
+    |
+=============''','''
+    +---+
+    |
+    |
+    |
+    |
+    |
+=============''','''
+    +
+    |
+    | 
+    |
+    |
+    |
+=============''','''
+
+
+    
+    
+    
+    
+=============''']
+
+
 import random
 word = ["UDEMY","LOVER"]
 secret = random.choice(word)
@@ -10,26 +71,29 @@ longity = len(secret)
 blank = []
 for space in range(longity):
     blank.append("_")
-print(blank)
+print(" ".join(blank))
 guessed = []
-lives = 6
+lives = 9
 end_games = False
 while end_games!=True:
     guess = input("Guess a letter : ").upper()
     if guess in guessed:
         print("You have already guessed this letter!")
+        continue
     else:
         guessed.append(guess)
+    print(guessed)
+    
     for position in range(longity):
         letter = secret[position]
         if guess==letter:
             blank[position] = letter
-    if guess not in secret_word:
+    if guess not in secret:
         lives -=1
     if lives == 0:
-        end_game = True
+        end_games = True
         print("You lose!")
-    print(blank)
+    print(" ".join(blank))
     if "_" not in blank:
-        end_game = True
+        end_games = True
         print("You win!")
