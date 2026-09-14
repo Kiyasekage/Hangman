aded595069de9836b0d8553746a8fa158e453939
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-09-15 04:28:18 +0800

    Addwhile loop to allow user attempt guess the word

--- file.py
+++ file.py
@@ -11,14 +11,18 @@ blank = []
 for space in range(longity):
     blank.append("_")
 print(blank)
-guess = input("Guess a letter : ").upper()
 guessed = []
-if guess in guessed:
-    print("You have already guessed this letter!")
-else:
-    guessed.append(guess)
-for position in range(longity):
-    letter = secret[position]
-    if guess==letter:
-        blank[position] = letter
-print(blank)
+end_games = False
+while end_games!=True:
+    guess = input("Guess a letter : ").upper()
+    if guess in guessed:
+        print("You have already guessed this letter!")
+    else:
+        guessed.append(guess)
+    for position in range(longity):
+        letter = secret[position]
+        if guess==letter:
+            blank[position] = letter
+    print(blank)
+    if "_" not in blank:
+        end_game = True
